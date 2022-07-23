99e693d76d5f1ab353998fd22470b137361b5dea
maxirama <maximilianogastonrama@gmail.com>
2022-07-23 15:06:44 -0300

    My first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div> Hola Mund </div>;
+  return <div> Hola Mundo</div>;
 }
 
 export default App;
